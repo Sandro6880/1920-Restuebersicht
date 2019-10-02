281f7a762326940f8a2675fd204a41b3a80df9f3
sandro6880 <sandro6880@gmail.com>
2019-10-02 23:09:58 +0200

    First Version from Time added

--- Restprimer/src/main/java/at/htl/restprimer/rest/TimeServerResource.java
+++ Restprimer/src/main/java/at/htl/restprimer/rest/TimeServerResource.java
@@ -3,12 +3,20 @@ package at.htl.restprimer.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Path("time")
 public class TimeServerResource {
 
     @GET
     public String time() {
-        return "Time : " + LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd,yyyy HH:mm:ss");
+        String formatDateTime;
+        if (now.getHour() > 12){
+            formatDateTime = now.format(formatter) + "PM";
+        }else  formatDateTime = now.format(formatter) + "AM";
+
+        return "Time : " + formatDateTime;
     }
 }
