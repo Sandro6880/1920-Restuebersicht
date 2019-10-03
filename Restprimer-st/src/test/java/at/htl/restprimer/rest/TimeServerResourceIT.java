8daf885db2ce67ac47603f49e6bb8a334b313747
sandro6880 <sandro6880@gmail.com>
2019-10-03 13:41:34 +0200

    configured the testfile

--- Restprimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
+++ Restprimer-st/src/test/java/at/htl/restprimer/rest/TimeServerResourceIT.java
@@ -1,4 +1,32 @@
 package at.htl.restprimer.rest;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+
 public class TimeServerResourceIT {
+    private Client client;
+    private WebTarget tut;
+
+    @BeforeAll
+    public void initClient(){
+        this.client = ClientBuilder.newClient();
+        this.tut = this.client.target("http://localhost:8080/restprimer/rs/time");
+
+    }
+    @Test
+    public void fetchTime(){
+        Response response = this.tut.request(MediaType.TEXT_PLAIN).get();
+        assertThat(response.getStatus(), is(200));
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.startsWith("Time :"));
+    }
 }
